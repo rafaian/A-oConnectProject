3d6da102a926307a32e92b1859dda82f16b020c6
rafaian <rafaelserafimx@gmail.com>
2026-02-09 10:45:08 -0300

    criação e estilização das paginas 'quem somos' e 'ações da connect'

--- src/components.jsx/AcoesDaConnect/AcoesDaConnect.jsx
+++ src/components.jsx/AcoesDaConnect/AcoesDaConnect.jsx
@@ -1,7 +1,88 @@
+import { useState } from "react"
+
+import Mutirao from '../../assets/mutirao.png'
+import Aulas from '../../assets/aulas.png'
+import Esporte from '../../assets/esporte.png'
+import Familia from '../../assets/familia.png'
+import Futuro from '../../assets/futuro.png'
+import Conecta from '../../assets/conecta.png'
+
+import styles from './AcoesDaConnect.module.scss'
+
+
 export default function AcoesDaConnect(){
+    const [cards] = useState([
+        {
+            id: 1,
+            imagem: Mutirao,
+            titulo: "Mutirão de Reciclagem",
+            informacao: "Coletar materiais recicláveis e orientar sobre descarte consciente.",
+            botao: "Quero participar"
+
+        },
+        {
+            id: 2,
+            imagem: Aulas,
+            titulo: "Aulas de Tecnologia",
+            informacao: "Ensinar noções básicas de informática, internet segura e programação. Preparar jovens para o mercado de trabalho digital.",
+            botao: "Quero participar"
+
+        },
+        {
+            id: 3,
+            imagem: Esporte,
+            titulo: "Esporte e Inclusão",
+            informacao: "Organizar treinos e jogos que incentivem a participação de todos. Promover disciplina, saúde e trabalho em equipe entre jovens.",
+            botao: "Quero participar"
+
+        },
+        {
+            id: 4,
+            imagem: Familia,
+            titulo: "Instituto Grande Família",
+            informacao: "Contribua com alimentos não perecíveis e ajude famílias em situação de vulnerabilidade.",
+            botao: "Quero participar"
+
+        },
+        {
+            id: 5,
+            imagem: Futuro,
+            titulo: "Projeto Futuro na Escola",
+            informacao: "Doe livros, cadernos, lápis, mochilas e canetas para ajudar jovens a continuarem seus estudos com mais estrutura.",
+            botao: "Quero participar"
+
+        },
+        {
+            id: 6,
+            imagem: Conecta,
+            titulo: "Instituto Conecta Jovem",
+            informacao: "Doe computadores usados, tablets ou celulares em bom estado para permitir que jovens tenham acesso ao mundo digital.",
+            botao: "Quero participar"
+
+        },
+      
+    ])
     return(
-        <>
-            <h1>Eu sou a Ações</h1>
-        </>
+        <section className={styles.containerDeAcoes}>
+                <div className={styles.boxTituloAcoes}>
+                    <h1>Eu sou a Ações</h1>
+                </div>
+        <section className={styles.containerCardsDeAcoes}>
+
+                {
+                    cards.map((item)=>(
+                        <article className={styles.cardsDeAcoes} key={item.id}>
+                            <img src={item.imagem} alt="" />
+                            <h2>{item.titulo}</h2>
+                            <p>{item.informacao}</p>
+                            <button>{item.botao}</button>
+                        </article>
+                    ))
+
+                }
+
+            </section>
+        </section>
+        
     )
 }
